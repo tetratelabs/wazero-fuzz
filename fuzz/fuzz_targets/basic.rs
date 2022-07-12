21721ffacf5eb9252d13be26d5572e03998972d5
Takeshi Yoneda <takeshi@tetrate.io>
2022-07-12 14:54:55 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- fuzz/fuzz_targets/basic.rs
+++ fuzz/fuzz_targets/basic.rs
@@ -5,8 +5,6 @@ use libfuzzer_sys::fuzz_target;
 use wasm_smith::SwarmConfig;
 
 fuzz_target!(|data: &[u8]| {
-    // errors in `run` have to do with not enough input in `data`, which we
-    // ignore here since it doesn't affect how we'd like to fuzz.
     drop(run(data));
 });
 
@@ -24,18 +22,22 @@ fn run(data: &[u8]) -> Result<()> {
     // TODO: after having CompiledModule.Imports(), enable this with dummy imports.
     // See https://github.com/bytecodealliance/wasmtime/blob/f242975c49385edafe4f72dfa5f0ff6aae23eda3/crates/fuzzing/src/oracles/dummy.rs#L6-L20
     config.max_imports = 0;
+    // If we don't set the limit, we will soon reach the OOM and the fuzzing will be killed by OS.
     config.max_memory_pages = 10;
-    config.reference_types_enabled = false;
 
-    let module = wasm_smith::Module::new(config.clone(), &mut u)?;
-    let module_byte = module.to_bytes();
+    // Generate the random module via wasm-smith.
+    let module_byte = wasm_smith::Module::new(config.clone(), &mut u)?.to_bytes();
 
+    // Pass the randomly generated module to the wazero library.
     unsafe {
         run_wazero(module_byte.as_ptr(), module_byte.len());
     }
+
+    // We always return Ok as inside of run_wazero, we cause panic if the binary is interesting.
     Ok(())
 }
 
 extern "C" {
+    // run_wazero is implemented in Go, and accepts the pointer to the binary and its size.
     fn run_wazero(binary_ptr: *const u8, binary_size: usize);
 }
